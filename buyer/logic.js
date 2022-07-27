a80af784f973cbbd36bee178c3e10f42d00d5ba5
mohmmed23 <mhmmedben3@gmail.com>
2022-07-27 11:17:05 +0300

    edit search fun #12

--- buyer/logic.js
+++ buyer/logic.js
@@ -1,4 +1,6 @@
 function search(array, value) {
-  let newArr = array.filter((item) => item.name.includes(value))
+  let newArr = array.filter((item) =>
+    item.name.toLowerCase().trim().includes(value.toLowerCase().trim()),
+  )
   return newArr
 }
